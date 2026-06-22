e7487ecf1bce7d5bb7c3ca95896bea7e1a4d3031
kombanking436 <kombanking436@gmail.com>
2026-06-22 09:36:06 +0530

    Update login.php

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ $error = '';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'] ?? '';
     $password = $_POST['password'] ?? '';
-    
+
     if (empty($email) || empty($password)) {
         $error = "Email and password are required.";
     } else {
